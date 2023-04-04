62c7130d8e563e3d1490375b0e80023335858187
Shashank Chandrasekaran <shashuchan@gmail.com>
2023-04-04 01:18:17 +0000

    Update to char driver  package makefile

--- base_external/package/aesd_ldd/aesd_ldd.mk
+++ base_external/package/aesd_ldd/aesd_ldd.mk
@@ -13,10 +13,7 @@ AESD_LDD_MODULE_SUBDIRS = aesd-char-driver/
 define AESD_LDD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
-	
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/drivertest.sh $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/assignment-test.sh $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/sockettest.sh $(TARGET_DIR)/usr/bin
+
 endef
 
 $(eval $(kernel-module))
