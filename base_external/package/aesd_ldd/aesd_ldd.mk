88db09a3d6482fd746ab84acc535dc98106e726c
Shashank Chandrasekaran <shashuchan@gmail.com>
2023-04-04 01:55:47 +0000

    Memory leak detector passed with no leaks

--- base_external/package/aesd_ldd/aesd_ldd.mk
+++ base_external/package/aesd_ldd/aesd_ldd.mk
@@ -13,7 +13,6 @@ AESD_LDD_MODULE_SUBDIRS = aesd-char-driver/
 define AESD_LDD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
-
 endef
 
 $(eval $(kernel-module))
